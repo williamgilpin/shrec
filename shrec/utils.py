77386aa88b2eb946290e530e7a2e7fb53555c8d0
williamgilpin <williamgilpin@gmail.com>
2022-08-11 04:03:29 -0500

    latest

--- shrec/utils.py
+++ shrec/utils.py
@@ -401,6 +401,108 @@ def community_list_to_labels(community_list):
 #     return all_pairs
 
 
+from sklearn.base import BaseEstimator, TransformerMixin
+
+class RigidTransform(BaseEstimator, TransformerMixin):
+    """
+    Compute the rigid transformation (rotation, translation, scaling) aligning two
+    sets of points using the Kabsch-Umeyama algorithm. The first argument is transformed
+    to align with the second argument.
+
+    Attributes:
+        scale (float): scaling factor
+        rotation (array): rotation matrix
+        translation (array): translation vector
+    """
+
+    def __init__(self, scale=None, rotation=None, translation=None):
+        self.scale = scale
+        self.rotation = rotation
+        self.translation = translation
+
+    def fit(self, X, y=None):
+        """
+        Fit transformation matrices that align X to y.
+
+        Args:
+            X: (N, M) array of points to be transformed
+            y: (N, M) array of points to align with
+        """
+        b, a = X, y
+        assert a.shape == b.shape
+        n, m = a.shape
+
+        ca = np.mean(a, axis=0)
+        cb = np.mean(b, axis=0)
+
+        var_a = np.mean(np.linalg.norm(a - ca, axis=1) ** 2)
+
+        H = ((a - ca).T @ (b - cb)) / n
+        U, D, VT = np.linalg.svd(H)
+        d = np.sign(np.linalg.det(U) * np.linalg.det(VT))
+        S = np.diag([1] * (m - 1) + [d])
+
+        R = U @ S @ VT
+        c = var_a / np.trace(np.diag(D) @ S)
+        t = ca - c * R @ cb
+
+        transform_params = (R, c, t)
+
+        self.rotation = R
+        self.scale = c
+        self.translation = t
+
+    def fit_transform(self, X, y=None):
+        """
+        Fit transformation matrices that align X to y and apply them to X.
+
+        Args:
+            X: (N, M) array of points to be transformed
+            y: (N, M) array of points to align with
+        """
+        self.fit(X, y)
+        return self.transform(X)
+
+    def transform(self, X):
+        X_transformed = self.scale * self.rotation @ X.T + self.translation[:, np.newaxis]
+        return X_transformed.T
+
+    def inverse_transform(self, X):
+        X = X.T
+        X = (X - self.translation[:, np.newaxis]) / self.scale
+        X = self.rotation.T @ X
+        return X.T
+
+    
+
+def array_select(arr, inds):
+    """
+    Selects a subset of an array, given a set of indices or boolean slices
+    """
+    arr_out = np.copy(arr)
+    arr_out = arr_out[inds]
+    arr_out = arr_out[:, inds]
+    return arr_out
+
+def dict_to_vals(d):
+    """
+    Convert a dictionary to an array of values, in the order of the sorted keys.
+
+    Args:
+        d (dict): dictionary to convert
+
+    Returns:
+        d_arr (np.array): array of values
+        key_names (list): list of keys
+    """
+    d_arr = list()
+    key_names = sorted(d.keys())
+    for key in key_names:
+        d_arr.append(d[key])
+    d_arr = np.array(d_arr)
+    return d_arr, key_names
+
+
 
 ## NetworkX utilities
 
