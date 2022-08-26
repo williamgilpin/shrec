8b9ced7962b36cb176db4a92131b66191cdc3abb
williamgilpin <williamgilpin@gmail.com>
2022-08-26 02:25:18 -0500

    latest

--- shrec/utils.py
+++ shrec/utils.py
@@ -215,6 +215,14 @@ def arg_find(search_vals, target_vals):
 def find_psd(y, window=True):
     """
     Find the power spectrum of a signal
+
+    Args:
+        y (array): A signal of shape (T,)
+
+    Returns:
+        fvals (array): The frequencies of the power spectrum with shape (T // 2,)
+        psd (array): The power spectrum of shape (T // 2,)
+
     """
     if window:
         y = y * blackmanharris(len(y))
@@ -473,6 +481,48 @@ class RigidTransform(BaseEstimator, TransformerMixin):
         X = self.rotation.T @ X
         return X.T
 
+
+from sklearn.preprocessing import PowerTransformer
+
+def make_surrogate(X, ns=1, method="random_phase", gaussianize=True, random_state=None):
+    """
+    Create a surrogate time series from a given time series. If gaussianize is True,
+    then the surrogate will be gaussianized beforehand. The default configuration 
+    approximates the AAFT surrogate method.
+    
+    Args:
+        X (ndarray): A one-dimensional time series
+        method (str): "random_shuffle" or "random_phase"
+        gaussianize (bool): If True, the surrogate will be gaussianized
+        random_state (int): Random seed for reproducibility
+        
+    Returns:
+        Xs (ndarray): A single random surrogate time series
+    """
+    if gaussianize:
+        model = PowerTransformer(standardize=True)
+        X = np.squeeze(model.fit_transform(X[:, None]))
+
+    np.random.seed(random_state)
+    if method == "random_phase":
+        phases, radii = np.angle(np.fft.fft(X)), np.abs(np.fft.fft(X))
+        random_phases = 2 * np.pi * (2 * (np.random.random(size=(phases.shape[0], ns)) - 0.5))
+        Xs = np.real(
+            np.fft.ifft(
+            radii[:, None] * np.cos(random_phases) + 1j * radii[:, None] * np.sin(random_phases),
+            axis=0
+            )
+        )
+    else:
+        Xs = np.random.permutation(X)
+
+    if gaussianize:
+        Xs = np.array([
+            model.inverse_transform(item[:, None]) for item in Xs.T
+        ]).T
+
+    Xs = np.squeeze(Xs)
+    return Xs
     
 
 def array_select(arr, inds):
